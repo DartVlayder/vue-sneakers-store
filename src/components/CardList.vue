1efef391fed74adbc3ac17e061fdfc43e8c0af4c
DartVlayder <mr.6990@mail.ru>
2024-03-07 14:02:07 +0300

    Сверстал шапку и карточку

---
+++ src/components/CardList.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import CardMain from './CardMain.vue'
+</script>
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <CardMain :price="5232" title="Кросы" image-url="/sneakers/sneakers-2.jpg" />
+  </div>
+</template>
