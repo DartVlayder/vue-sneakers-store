41b0a369a6d6a38a16dfac835fa012d1af8c1984
DartVlayder <mr.6990@mail.ru>
2024-04-07 19:27:47 +0300

    Разработал поиск и фильтрацию товаров

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,26 @@ import Drawer from './components/Drawer.vue'
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <Header />
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кросовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кросовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-mb outline-none">
+            <option>По названию</option>
+            <option>По цене (дешёвые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-3 top-3" src="/search.svg" alt="поиск" />
+            <input
+              class="border rounded-mb py-2 pl-12 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
       <CardList />
     </div>
   </div>
