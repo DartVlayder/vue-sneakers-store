b1d72b8e3026cbdb294800c53540050203d9c0b1
DartVlayder <mr.6990@mail.ru>
2024-04-07 21:06:40 +0300

    Разработал базу данных на mokky и получил оттуда данные

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <script setup lang="ts">
+import { onMounted, reactive, ref, watch } from 'vue'
+import axios from 'axios'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+    const { data } = await axios.get(`https://f9bd9a56dceb3ac3.mokky.dev/items`, {
+      params
+    })
+    console.log(data)
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 <template>
   <!-- <Drawer /> -->
@@ -12,15 +48,16 @@ import Drawer from './components/Drawer.vue'
         <h2 class="text-3xl font-bold mb-8">Все кросовки</h2>
 
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-mb outline-none">
-            <option>По названию</option>
-            <option>По цене (дешёвые)</option>
-            <option>По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-mb outline-none">
+            <option value="name">По названию</option>
+            <option value="price">Сначала дешевле</option>
+            <option value="-price">Сначала дороже</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-3 top-3" src="/search.svg" alt="поиск" />
             <input
+              @input="onChangeSearchInput"
               class="border rounded-mb py-2 pl-12 pr-4 outline-none focus:border-gray-400"
               type="text"
               placeholder="Поиск..."
@@ -28,7 +65,9 @@ import Drawer from './components/Drawer.vue'
           </div>
         </div>
       </div>
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
 import Card from './Card.vue'
+import { defineProps, defineEmits } from 'vue'
+
+defineProps<{
+  items: Array<{
+    id: number
+    price: number
+    title: string
+    imageUrl: string
+  }>
+}>()
 </script>
+
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card :price="5232" title="Кросы" image-url="/sneakers/sneakers-2.jpg" />
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :price="item.price"
+      :title="item.title"
+      :imageUrl="item.imageUrl"
+      clickAdd="onClickAdd"
+    />
   </div>
 </template>
